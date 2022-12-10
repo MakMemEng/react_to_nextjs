6513f8ce9c7f0cbaa8f82f08546dfcb09a182f78
MakMemEng <yatani.mems4568@gmail.com>
2022-12-10 16:14:53 +0900

    ⚡️ useCallback, useEffectの第2引数の挙動確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,16 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from "react";
-import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
 
 const Home = () => {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count]);
 
   // useEffect: マウント時とアンマウント時の処理を切り分けする
   // Home Componentがマウントされるタイミングで実行
@@ -19,6 +20,7 @@ const Home = () => {
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
 
+    // CleanUp Function
     // アンマウントするタイミングで実行
     return () => {
       document.body.style.backgroundColor = "";
