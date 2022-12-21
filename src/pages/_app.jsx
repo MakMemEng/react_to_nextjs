8fb66262778b7b136d744153d2ee75d52a3f022c
MakMemEng <yatani.mems4568@gmail.com>
2022-12-22 07:06:16 +0900

    ✨ pages間のStateのLiftUpを使用

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,21 @@
-import Head from "next/head";
 import "src/styles/globals.css";
+import Head from "next/head";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 const MyApp = ({ Component, pageProps }) => {
+  // counter = { count, isShow, handleClick, handleDisplay }
+  const counter = useCounter();
+  // inputArray = { text, array, handleChange, handleAdd }
+  const inputArray = useInputArray();
+  useBgLightBlue();
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} {...counter} {...inputArray} />;
     </>
   );
 };
